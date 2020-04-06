b18e589b79f64a23a6933932579555ff7b488085
Junmin Ahn <junmin@button.is>
2020-04-06 10:12:34 -0700

    create only one docker container to test

--- docker/stream-minio/init.sh
+++ docker/stream-minio/init.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-FILE_URLS=$(sh bin/wget-spider.sh | awk '{printf "--url=\"%s\" ",$0}')
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 <bucket_name> [<file_path>]"
+    exit 0
+fi
+
+if [ -z "$2" ]; then
+    FILE_URLS=$(sh bin/wget-spider.sh | awk '{printf "--url=\"%s\" ",$0}')
+else
+    cat $2
+    FILE_URLS=$(cat $2 | awk '{printf "--url=\"%s\" ",$0}')
+fi
+
 node stream-minio --ssl --bucket="$1" $FILE_URLS

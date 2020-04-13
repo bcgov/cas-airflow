a5f18f50c88d74fb4ab8f18bb7bf39ff36c11b89
Junmin Ahn <junmin@button.is>
2020-04-13 10:22:45 -0700

    user environment vars for the scripts

--- dags/stream_minio_kubernetes_pod.py
+++ dags/stream_minio_kubernetes_pod.py
@@ -25,6 +25,15 @@ namespace = conf.get('kubernetes', 'NAMESPACE')
 
 compute_resource = {'request_cpu': '800m', 'request_memory': '3Gi', 'limit_cpu': '800m', 'limit_memory': '3Gi'}
 
+env_vars = {
+    'DEPTH': os.getenv('SWRS_DEPTH'),
+    'WEBSITE': os.getenv('SWRS_WEBSITE'),
+    'FILTER': os.getenv('SWRS_FILTER'),
+    'MINIO_ACCESS_KEY': os.getenv('MINIO_ACCESS_KEY'),
+    'MINIO_SECRET_KEY': os.getenv('MINIO_SECRET_KEY'),
+    'MINIO_HOST': os.getenv('MINIO_HOST'),
+}
+
 with dag:
     k = KubernetesPodOperator(
         task_id=DAG_ID,
@@ -33,9 +42,9 @@ with dag:
         image="docker.pkg.github.com/bcgov/cas-airflow-dags/stream-minio:lasted",
         cmds=["./init.sh"],
         arguments=[],
-        env_vars={'MINIO_HOST': 'cas-minio-wksv3k-dev.pathfinder.gov.bc.ca'},
+        env_vars=env_vars,
         resources=compute_resource,
         is_delete_operator_pod=True,
         get_logs=True,
-        in_cluster=False,
+        in_cluster=True,
         do_xcom_push=False)

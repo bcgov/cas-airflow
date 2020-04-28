cc172de58963ade2df16d16e56d5ce1be4001c72
Junmin Ahn <junmin@button.is>
2020-04-28 09:51:34 -0700

    test minio image

--- dags/stream_minio_kubernetes_pod.py
+++ dags/stream_minio_kubernetes_pod.py
@@ -21,29 +21,32 @@ SCHEDULE_INTERVAL = '0 0 * * *'
 
 dag = DAG(DAG_ID, schedule_interval=SCHEDULE_INTERVAL, default_args=default_args)
 
+namespace = conf.get('kubernetes', 'NAMESPACE')
+
+compute_resource = {'request_cpu': '800m', 'request_memory': '3Gi', 'limit_cpu': '800m', 'limit_memory': '3Gi'}
+
+env_vars = {
+    'DEPTH': os.getenv('SWRS_DEPTH'),
+    'WEBSITE': os.getenv('SWRS_WEBSITE'),
+    'FILTER': os.getenv('SWRS_FILTER'),
+    'MINIO_ACCESS_KEY': os.getenv('MINIO_ACCESS_KEY'),
+    'MINIO_SECRET_KEY': os.getenv('MINIO_SECRET_KEY'),
+    'MINIO_HOST': os.getenv('MINIO_HOST'),
+}
+
+image = "docker.pkg.github.com/bcgov/cas-airflow-dags/stream-minio:" + os.getenv('STREAM_MINIO_IMAGE_TAG')
+
 with dag:
-    k =KubernetesPodOperator(
-        # The ID specified for the task.
-        task_id='pod-ex-minimum',
-        # Name of task you want to run, used to generate Pod ID.
-        name='pod-ex-minimum',
-        # Entrypoint of the container, if not specified the Docker container's
-        # entrypoint is used. The cmds parameter is templated.
-        cmds=['echo'],
-        # The namespace to run within Kubernetes, default namespace is
-        # `default`. There is the potential for the resource starvation of
-        # Airflow workers and scheduler within the Cloud Composer environment,
-        # the recommended solution is to increase the amount of nodes in order
-        # to satisfy the computing requirements. Alternatively, launching pods
-        # into a custom namespace will stop fighting over resources.
-        namespace='wksv3k-tools',
-        # Docker image specified. Defaults to hub.docker.com, but any fully
-        # qualified URLs will point to a custom repository. Supports private
-        # gcr.io images if the Composer Environment is under the same
-        # project-id as the gcr.io images and the service account that Composer
-        # uses has permission to access the Google Container Registry
-        # (the default service account has permission)
-        image='ubuntu:16.04',
-        in_cluster=True, # if set to true, will look in the cluster, if false, looks for file
+    k = KubernetesPodOperator(
+        task_id=DAG_ID,
+        name=DAG_ID,
+        namespace=namespace,
+        image=image,
+        cmds=["./init.sh"],
+        arguments=["swrs-import"],
+        env_vars=env_vars,
+        resources=compute_resource,
         is_delete_operator_pod=True,
-        get_logs=True)
+        get_logs=True,
+        in_cluster=True,
+        do_xcom_push=False)

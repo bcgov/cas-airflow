42b03da670ac85d2c70ef9363f5337c8f36ef367
Junmin Ahn <junmin@button.is>
2020-04-29 15:15:08 -0700

    fix shellcheck

--- docker/stream-minio/init.sh
+++ docker/stream-minio/init.sh
@@ -15,7 +15,7 @@ if [ -z "$2" ]; then
     echo "$FILE_URLS"
 else
     cat "$2"
-    FILE_URLS="$(awk '{printf "--url=\"%s\" ",$0}' < "$2")"
+    FILE_URLS="$("$2" | awk '{printf "--url=\"%s\" ",$0}')"
 fi
 
 node stream-minio --ssl --bucket="$1" "$FILE_URLS"

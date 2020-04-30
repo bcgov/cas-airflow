3479b4ba4b788bae747c0df21b3b847e15e686c8
Dylan Leard <dylan@button.is>
2020-04-30 13:14:29 -0700

    fix: fixed a typo in the task id which caused the dag to break

--- dags/remove-child-process-logs.py
+++ dags/remove-child-process-logs.py
@@ -8,7 +8,7 @@ dag = DAG('remove_child_process_logs_dag', description='Delete log files',
           start_date=datetime(2017, 3, 20), catchup=False)
 
 run_remove_child_process_logs = BashOperator(
-    task_id='delete _task',
+    task_id='delete_task',
     bash_command='rm -rf ./logs/scheduler',
     dag=dag)
 

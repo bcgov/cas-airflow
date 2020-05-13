e72123a6dce134e072df9c5e2e538f0df721a9a3
Matthieu Foucault <matthieu@button.is>
2020-05-13 16:48:26 -0700

    feat: write XML files to ggircs database

--- dags/stream_minio_kubernetes_pod.py
+++ dags/stream_minio_kubernetes_pod.py
@@ -35,7 +35,9 @@ env_vars = {
     'MINIO_HOST': os.getenv('MINIO_HOST'),
 }
 
-image = "docker.pkg.github.com/bcgov/cas-airflow-dags/stream-minio:" + os.getenv('STREAM_MINIO_IMAGE_TAG')
+stream_minio_image = "docker.pkg.github.com/bcgov/cas-airflow-dags/stream-minio:" + os.getenv('STREAM_MINIO_IMAGE_TAG')
+extract_zips_image = "docker.pkg.github.com/bcgov/cas-airflow-dags/extract-zips-to-ggircs:" + os.getenv('EXTRACT_ZIPS_IMAGE_TAG')
+
 namespace = os.getenv('NAMESPACE')
 
 def should_extract_zips(**context):
@@ -47,14 +49,14 @@ with dag:
         task_id='download_eccc_files',
         name='download_eccc_files',
         namespace=namespace,
-        image=image,
+        image=stream_minio_image,
         cmds=["./init.sh"],
         arguments=["swrs-import"],
         env_vars=env_vars,
         resources=compute_resource,
         is_delete_operator_pod=True,
         get_logs=True,
-        in_cluster=True,
+        in_cluster=False, ### DON'T MERGE THIS?
         do_xcom_push=True)
 
     should_extract_zips_op = ShortCircuitOperator(
@@ -62,4 +64,61 @@ with dag:
         provide_context=True,
         python_callable=should_extract_zips)
 
+    extract_zips_to_ggircs = KubernetesPodOperator(
+        task_id='extract_zips_to_ggircs',
+        name='extract_zips_to_ggircs',
+        namespace=namespace,
+        image=image,
+        env_vars={
+            'DOWNLOAD_ECCC_FILES_XCOM': '{{ task_instance.xcom_pull(task_ids="download_eccc_files", key="return_value") }}'
+        },
+        resources=compute_resource,
+        is_delete_operator_pod=True,
+        get_logs=True,
+        in_cluster=False, ### DON'T MERGE THIS?
+        do_xcom_push=True)
+
+    load_ggircs_swrs = KubernetesPodOperator( # TODO
+        task_id='load_ggircs_swrs',
+        name='load_ggircs_swrs',
+        namespace=namespace,
+        image=image,
+        env_vars={
+            'DOWNLOAD_ECCC_FILES_XCOM': '{{ task_instance.xcom_pull(task_ids="download_eccc_files", key="return_value") }}'
+        },
+        resources=compute_resource,
+        is_delete_operator_pod=True,
+        get_logs=True,
+        in_cluster=False, ### DON'T MERGE THIS?
+        do_xcom_push=True)
+
+    load_portal_swrs = KubernetesPodOperator( # TODO
+        task_id='load_ggircs_swrs',
+        name='load_ggircs_swrs',
+        namespace=namespace,
+        image=image,
+        env_vars={
+            'DOWNLOAD_ECCC_FILES_XCOM': '{{ task_instance.xcom_pull(task_ids="download_eccc_files", key="return_value") }}'
+        },
+        resources=compute_resource,
+        is_delete_operator_pod=True,
+        get_logs=True,
+        in_cluster=False, ### DON'T MERGE THIS?
+        do_xcom_push=True)
+
+    update_portal_facilities_operators = KubernetesPodOperator( # TODO
+        task_id='load_ggircs_swrs',
+        name='load_ggircs_swrs',
+        namespace=namespace,
+        image=image,
+        env_vars={
+            'DOWNLOAD_ECCC_FILES_XCOM': '{{ task_instance.xcom_pull(task_ids="download_eccc_files", key="return_value") }}'
+        },
+        resources=compute_resource,
+        is_delete_operator_pod=True,
+        get_logs=True,
+        in_cluster=False, ### DON'T MERGE THIS?
+        do_xcom_push=True)
+
+    should_extract_zips_op >> extract_zips_to_ggircs
     download_eccc_files >> should_extract_zips_op
--- docker/extract-zips-to-ggircs/app/index.js
+++ docker/extract-zips-to-ggircs/app/index.js
@@ -1,20 +1,36 @@
 const argv = require('yargs').argv;
 const unzipperDirectory = require('unzipper/lib/Open/directory');
-const fs = require('fs');
 const {Storage} = require('@google-cloud/storage');
-const {pipeline} = require('stream');
+const chardet = require('chardet');
 const pg = require('pg');
+const md5 = require('md5');
 
-const storage = new Storage({keyFilename: 'gcs_key.json'});
-
-const DOWNLOAD_ECCC_FILES_XCOM =
-  argv.download_eccc_files_xcom || process.env.DOWNLOAD_ECCC_FILES_XCOM;
-
-const ECCC_ZIP_PASSWORDS =
-  argv.eccc_zip_passwords || process.env.ECCC_ZIP_PASSWORDS;
+const pgPool = new pg.Pool(); // Uses libpq env variables for connection information
 
 /* eslint-disable no-await-in-loop */
 (async function () {
+  if (!process.env.GCS_KEY) {
+    console.log('GCS_KEY env variable is required');
+    return;
+  }
+
+  const {projectId, client_email, private_key} = JSON.parse(
+    process.env.GCS_KEY
+  );
+
+  const storage = new Storage({
+    projectId,
+    credentials: {
+      client_email,
+      private_key
+    }
+  });
+
+  const DOWNLOAD_ECCC_FILES_XCOM =
+    argv.download_eccc_files_xcom || process.env.DOWNLOAD_ECCC_FILES_XCOM;
+
+  const ECCC_ZIP_PASSWORDS =
+    argv.eccc_zip_passwords || process.env.ECCC_ZIP_PASSWORDS;
   if (!DOWNLOAD_ECCC_FILES_XCOM) {
     console.log('DOWNLOAD_ECCC_FILES_XCOM env variable is required');
     return;
@@ -39,9 +55,12 @@ const ECCC_ZIP_PASSWORDS =
     const zipFile = storage.bucket(bucketName).file(objectName);
     await processZipFile(zipFile, zipPasswords);
   }
-})();
+})().catch((error) => console.error(error.stack));
 
 async function processZipFile(zipFile, zipPasswords, passwordIndex = -1) {
+  let shouldTryNextPassword = false;
+  const metadata = await zipFile.getMetadata();
+  const zipFileMd5 = Buffer.from(metadata[0].md5Hash, 'base64').toString('hex');
   const directorySource = {
     async size() {
       const metadata = await zipFile.getMetadata();
@@ -66,35 +85,77 @@ async function processZipFile(zipFile, zipPasswords, passwordIndex = -1) {
     password = zipPasswords[passwordIndex];
   }
 
+  const directory = await unzipperDirectory(directorySource, {crx: true});
+  const pgClient = await pgPool.connect();
   try {
-    const directory = await unzipperDirectory(directorySource, {crx: true});
+    await pgClient.query('begin');
+
+    const insertZipFileResult = await pgClient.query(
+      `insert into swrs_extract.eccc_zip_file(zip_file_name, zip_file_md5_hash)
+      values ($1, $2)
+      on conflict(zip_file_md5_hash) do update set zip_file_name=excluded.zip_file_name
+      returning id`,
+      [zipFile.name, zipFileMd5]
+    );
+    const zipFileId = insertZipFileResult.rows[0].id;
+
     for (const file of directory.files) {
       if (file.path.toLowerCase().endsWith('.xml')) {
         console.log(`Streaming ${file.path}`);
-        await streamFile(file, password);
+        const xmlReportBuffer = await streamFile(file, password);
+        const encoding = chardet.detect(xmlReportBuffer);
+        const xmlReportMd5 = md5(xmlReportBuffer);
+        await pgClient.query(
+          `insert into swrs_extract.eccc_xml_file(xml_file, xml_file_name, xml_file_md5_hash, zip_file_id)
+          values ($1, $2, $3, $4)
+          on conflict(xml_file_md5_hash) do update set
+          xml_file=excluded.xml_file,
+          xml_file_name=excluded.xml_file_name,
+          zip_file_id=excluded.zip_file_id
+          `,
+          [
+            xmlReportBuffer.toString(encoding),
+            file.path,
+            xmlReportMd5,
+            zipFileId
+          ]
+        );
       } else {
         console.log(`Skipping ${file.path}`);
       }
     }
+
+    await pgClient.query('commit');
   } catch (error) {
+    await pgClient.query('rollback');
     if (error.message === 'MISSING_PASSWORD') {
       console.log(`${zipFile.name} needs a password`);
+      shouldTryNextPassword = true;
     } else if (error.message === 'BAD_PASSWORD') {
       console.log(
         `${zipFile.name} failed to open with password #${passwordIndex + 1}`
       );
+      shouldTryNextPassword = true;
     } else throw error;
-
-    processZipFile(zipFile, zipPasswords, passwordIndex + 1);
+  } finally {
+    pgClient.release();
   }
+
+  if (shouldTryNextPassword)
+    await processZipFile(zipFile, zipPasswords, passwordIndex + 1);
 }
 
 function streamFile(file, password) {
   return new Promise((resolve, reject) => {
-    pipeline(file.stream(password), fs.createWriteStream(file.path), (err) => {
-      if (err) reject(err);
-      else resolve();
-    });
+    const chunks = [];
+    file
+      .stream(password)
+      .on('error', reject)
+      .on('data', (chunk) => chunks.push(chunk))
+      .on('end', () => {
+        const buffer = Buffer.concat(chunks);
+        resolve(buffer);
+      });
   });
 }
 

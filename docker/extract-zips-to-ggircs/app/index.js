30025d63577f117a2cde481e232e3145df1edb2f
Matthieu Foucault <matthieu@button.is>
2020-05-13 11:11:38 -0700

    feat: attempt open zip files with an array of passwd (and without pass)

--- docker/extract-zips-to-ggircs/app/index.js
+++ docker/extract-zips-to-ggircs/app/index.js
@@ -1,64 +1,101 @@
-// Const Minio = require('minio');
 const argv = require('yargs').argv;
-const unzipper = require('unzipper');
 const unzipperDirectory = require('unzipper/lib/Open/directory');
-const etl = require('etl');
-const {Storage} = require('@google-cloud/storage');
 const fs = require('fs');
+const {Storage} = require('@google-cloud/storage');
+const {pipeline} = require('stream');
+const pg = require('pg');
 
-// Creates a client
 const storage = new Storage({keyFilename: 'gcs_key.json'});
 
-// Const MINIO_ACCESS_KEY = argv.minio_access_key || process.env.MINIO_ACCESS_KEY;
-// const MINIO_SECRET_KEY = argv.minio_secret_key || process.env.MINIO_SECRET_KEY;
-// const MINIO_PORT =
-//   argv.minio_port ||
-//   (process.env.MINIO_PORT && Number.parseInt(process.env.MINIO_PORT, 10));
-// const MINIO_SSL = argv.minio_ssl || process.env.MINIO_SSL === 'true';
-// const MINIO_HOST = argv.minio_host || process.env.MINIO_HOST;
-const DOWNLOAD_ECCC_FILES_XCOM = JSON.parse(
-  argv.download_eccc_files_xcom || process.env.DOWNLOAD_ECCC_FILES_XCOM
-);
+const DOWNLOAD_ECCC_FILES_XCOM =
+  argv.download_eccc_files_xcom || process.env.DOWNLOAD_ECCC_FILES_XCOM;
 
-// Const minioClient = new Minio.Client({
-//   endPoint: MINIO_HOST,
-//   port: MINIO_PORT,
-//   useSSL: MINIO_SSL,
-//   accessKey: MINIO_ACCESS_KEY,
-//   secretKey: MINIO_SECRET_KEY
-// });
+const ECCC_ZIP_PASSWORDS =
+  argv.eccc_zip_passwords || process.env.ECCC_ZIP_PASSWORDS;
 
+/* eslint-disable no-await-in-loop */
 (async function () {
-  for (const {
-    bucketName,
-    objectName
-  } of DOWNLOAD_ECCC_FILES_XCOM.uploadedObjects) {
+  if (!DOWNLOAD_ECCC_FILES_XCOM) {
+    console.log('DOWNLOAD_ECCC_FILES_XCOM env variable is required');
+    return;
+  }
+
+  if (!ECCC_ZIP_PASSWORDS) {
+    console.log('ECCC_ZIP_PASSWORDS env variable is required');
+    return;
+  }
+
+  const {uploadedObjects} = JSON.parse(DOWNLOAD_ECCC_FILES_XCOM);
+  const zipPasswords = JSON.parse(ECCC_ZIP_PASSWORDS);
+  for (const {bucketName, objectName} of uploadedObjects) {
+    if (!objectName || !bucketName) continue;
+    if (!objectName.endsWith('.zip')) {
+      console.log(
+        `${objectName} was uploaded in the bucket ${bucketName}, but that doesn't look like a zip file. Skipping.`
+      );
+      continue;
+    }
+
     const zipFile = storage.bucket(bucketName).file(objectName);
-    const directorySource = {
-      async size() {
-        const metadata = await zipFile.getMetadata();
-        console.log(metadata[0].size);
-        return metadata[0].size;
-      },
-      stream(offset, length) {
-        console.log(offset, length, offset + length);
-        return zipFile.createReadStream({
-          start: offset,
-          end: length && offset + length
-        });
-      }
-    };
-    const directory = await unzipperDirectory(directorySource, {crx: true});
-    directory.files.forEach((f) => console.log(f.path));
-    // Const objectStream = storage
-    //   .bucket(bucketName)
-    //   .file(objectName)
-    //   .createReadStream();
-    // objectStream.pipe(unzipper.Open.s3()).pipe(
-    //   etl.map((entry) => {
-    //     console.log(entry.path);
-    //     entry.autodrain();
-    //   })
-    // );
+    await processZipFile(zipFile, zipPasswords);
   }
 })();
+
+async function processZipFile(zipFile, zipPasswords, passwordIndex = -1) {
+  const directorySource = {
+    async size() {
+      const metadata = await zipFile.getMetadata();
+      return metadata[0].size;
+    },
+    stream(offset, length) {
+      return zipFile.createReadStream({
+        start: offset,
+        end: length && offset + length
+      });
+    }
+  };
+  let password;
+  if (passwordIndex === -1) {
+    console.log(`Opening ${zipFile.name} without password `);
+  } else if (passwordIndex >= zipPasswords.length) {
+    throw new Error(
+      `Tried all passwords to open ${zipFile.name}, but none worked.`
+    );
+  } else {
+    console.log(`Opening ${zipFile.name} with password #${passwordIndex + 1}`);
+    password = zipPasswords[passwordIndex];
+  }
+
+  try {
+    const directory = await unzipperDirectory(directorySource, {crx: true});
+    for (const file of directory.files) {
+      if (file.path.toLowerCase().endsWith('.xml')) {
+        console.log(`Streaming ${file.path}`);
+        await streamFile(file, password);
+      } else {
+        console.log(`Skipping ${file.path}`);
+      }
+    }
+  } catch (error) {
+    if (error.message === 'MISSING_PASSWORD') {
+      console.log(`${zipFile.name} needs a password`);
+    } else if (error.message === 'BAD_PASSWORD') {
+      console.log(
+        `${zipFile.name} failed to open with password #${passwordIndex + 1}`
+      );
+    } else throw error;
+
+    processZipFile(zipFile, zipPasswords, passwordIndex + 1);
+  }
+}
+
+function streamFile(file, password) {
+  return new Promise((resolve, reject) => {
+    pipeline(file.stream(password), fs.createWriteStream(file.path), (err) => {
+      if (err) reject(err);
+      else resolve();
+    });
+  });
+}
+
+/* eslint-enable no-await-in-loop */

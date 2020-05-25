96fb37f43864f27d94886a90cd3d481fb90f2c01
Matthieu Foucault <matthieu@button.is>
2020-05-25 14:12:43 -0700

    ci: add shipit config

---
+++ helm/cas-airflow/patch-values.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+dir_path=$(dirname "$(realpath $0)")
+
+if [ -z "$ENVIRONMENT" ]; then
+  echo "The ENVIRONMENT variable is not defined. Exiting."
+  exit 1;
+fi
+
+# TODO: get these values from the openshift command line
+declare -A uids=( ["wksv3k-dev"]="1009900000" ["wksv3k-test"]="1009960000" ["wksv3k-prod"]="1009930000")
+
+sed -i "s/\(namespace: &namespace\) .*/\1 $ENVIRONMENT/" "$dir_path/values.yaml"
+sed -i "s/\(namespace-uid: &namespace-uid\) .*/\1 '${uids[$ENVIRONMENT]}'/" "$dir_path/values.yaml"
+sed -i "s/\(gcs-folder: &gcs-folder\) .*/\1 gs:\/\/$ENVIRONMENT-cas-airflow/" "$dir_path/values.yaml"

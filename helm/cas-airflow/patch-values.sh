08ae9744ffff4df30014fbed4a73fdf78f11cd31
Matthieu Foucault <matthieu@button.is>
2020-05-25 14:32:25 -0700

    ci: shipit deploys the image tagged with the commit SHA1

--- helm/cas-airflow/patch-values.sh
+++ helm/cas-airflow/patch-values.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
-dir_path=$(dirname "$(realpath $0)")
+dir_path=$(dirname "$(realpath "$0")")
 
 if [ -z "$ENVIRONMENT" ]; then
   echo "The ENVIRONMENT variable is not defined. Exiting."
+  exit 1;
+fi
+
+if [ -z "$REVISION" ]; then
+  echo "The REVISION variable is not defined. Exiting."
   exit 1;
 fi
 
@@ -13,3 +18,4 @@ declare -A uids=( ["wksv3k-dev"]="1009900000" ["wksv3k-test"]="1009960000" ["wks
 sed -i "s/\(namespace: &namespace\) .*/\1 $ENVIRONMENT/" "$dir_path/values.yaml"
 sed -i "s/\(namespace-uid: &namespace-uid\) .*/\1 '${uids[$ENVIRONMENT]}'/" "$dir_path/values.yaml"
 sed -i "s/\(gcs-folder: &gcs-folder\) .*/\1 gs:\/\/$ENVIRONMENT-cas-airflow/" "$dir_path/values.yaml"
+sed -i "s/\(airflow-tag: &airflow-tag\) .*/\1 $REVISION/" "$dir_path/values.yaml"

299e12cd8b9b7c455a58988e9f3689ce6cba20cb
Dylan Leard <dylan@button.is>
2020-06-01 14:08:11 -0700

    feat: add exec_in_pod function

--- dags/full_walg_postgres_backup.py
+++ dags/full_walg_postgres_backup.py
@@ -4,12 +4,8 @@
 """
 from airflow import DAG
 from datetime import datetime, timedelta
-from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
-from airflow.operators.python_operator import ShortCircuitOperator
-from airflow.contrib.kubernetes.secret import Secret
-from airflow.contrib.kubernetes.volume import Volume
-from airflow.contrib.kubernetes.volume_mount import VolumeMount
-from airflow.hooks.base_hook import BaseHook
+from airflow.operators.python_operator import PythonOperator
+from dags.exec_in_pod import exec_in_pod
 
 import os
 import json
@@ -32,20 +28,13 @@ default_args = {
 DAG_ID = os.path.basename(__file__).replace(".pyc", "").replace(".py", "")
 SCHEDULE_INTERVAL = null
 make_backup = DAG(DAG_ID, default_args=default_args)
-compute_resource = {'request_cpu': '1', 'request_memory': '2Gi', 'limit_cpu': '2', 'limit_memory': '4Gi'}
-postgres_image = "docker-registry.default.svc:5000/wksv3k-dev/cas-postgres:latest"
 
-make_backup = KubernetesPodOperator(
-    task_id='make_full_postgres_backup',
-    name='make_full_postgres_backup',
-    namespace=namespace,
-    image=postgres_image,
-    cmds=["wal-g backup-push"],
-    arguments=["$PGDATA"],
-    resources=compute_resource,
-    is_delete_operator_pod=True,
-    get_logs=True,
-    in_cluster=in_cluster,
-    dag=make_backup)
+def exec_backup_in_pod(dag):
+    return PythonOperator(
+        python_callable=exec_in_pod,
+        task_id='make_full_postgres_backup',
+        op_args=['cas-ciip-postgres-master', namespace, 'wal-g backup-push $PGDATA'],
+        dag=dag
+    )
 
-make_backup
+exec_backup_in_pod(make_backup)

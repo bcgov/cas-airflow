83d54dd9ec9589be397acb8b1c1c45cbac0674dd
Matthieu Foucault <matthieu@button.is>
2020-07-03 10:53:00 -0700

    feat: add dag to trigger ciip portal acme issue job

--- dags/ciip_deploy_db.py
+++ dags/ciip_deploy_db.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-# DAGs to fetch and extract SWRS data from the ECCC website.
-swrs_eccc_import_full will download and extract all zip files in the GCS bucket
-swrs_eccc_import_incremental will only download and extract files that were uploaded in the first task of the DAG
+# DAGs to deploy the ciip portal.
+ciip_deploy_db will initialize the portal database and import the swrs data
+cron_acme_issue will issue a certificate for the CIIP portal
 """
 from airflow import DAG
 from datetime import datetime, timedelta
@@ -16,16 +16,15 @@ import json
 YESTERDAY = datetime.now() - timedelta(days=1)
 
 namespace = os.getenv('NAMESPACE')
-in_cluster = os.getenv('LOCAL_AIRFLOW', False) == False
 
 default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': YESTERDAY,
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+  'owner': 'airflow',
+  'depends_on_past': False,
+  'start_date': YESTERDAY,
+  'email_on_failure': False,
+  'email_on_retry': False,
+  'retries': 1,
+  'retry_delay': timedelta(minutes=5)
 }
 
 DAG_ID = os.path.basename(__file__).replace(".pyc", "").replace(".py", "")
@@ -34,33 +33,41 @@ SCHEDULE_INTERVAL = None
 dag = DAG(DAG_ID, schedule_interval=SCHEDULE_INTERVAL, default_args=default_args)
 
 ciip_portal_init_db = PythonOperator(
-        python_callable=trigger_k8s_cronjob,
-        task_id='ciip_portal_db_init',
-        op_args=['cas-ciip-portal-init-db', namespace],
-        dag=dag)
+  python_callable=trigger_k8s_cronjob,
+  task_id='ciip_portal_db_init',
+  op_args=['cas-ciip-portal-init-db', namespace],
+  dag=dag)
 
 ciip_portal_swrs_import = PythonOperator(
-        python_callable=trigger_k8s_cronjob,
-        task_id='ciip_portal_swrs_import',
-        op_args=['cas-ciip-portal-swrs-import', namespace],
-        dag=dag)
+  python_callable=trigger_k8s_cronjob,
+  task_id='ciip_portal_swrs_import',
+  op_args=['cas-ciip-portal-swrs-import', namespace],
+  dag=dag)
 
 ciip_portal_deploy_data = PythonOperator(
-        python_callable=trigger_k8s_cronjob,
-        task_id='ciip_portal_deploy_data',
-        op_args=['cas-ciip-portal-schema-deploy-data', namespace],
-        dag=dag)
+  python_callable=trigger_k8s_cronjob,
+  task_id='ciip_portal_deploy_data',
+  op_args=['cas-ciip-portal-schema-deploy-data', namespace],
+  dag=dag)
 
 ciip_portal_graphile_schema = PythonOperator(
-        python_callable=trigger_k8s_cronjob,
-        task_id='ciip_portal_graphile_schema',
-        op_args=['cas-ciip-portal-init-graphile-schema', namespace],
-        dag=dag)
+  python_callable=trigger_k8s_cronjob,
+  task_id='ciip_portal_graphile_schema',
+  op_args=['cas-ciip-portal-init-graphile-schema', namespace],
+  dag=dag)
 
 ciip_portal_app_user = PythonOperator(
-        python_callable=trigger_k8s_cronjob,
-        task_id='ciip_portal_app_user',
-        op_args=['cas-ciip-portal-app-user', namespace],
-        dag=dag)
+  python_callable=trigger_k8s_cronjob,
+  task_id='ciip_portal_app_user',
+  op_args=['cas-ciip-portal-app-user', namespace],
+  dag=dag)
 
 ciip_portal_init_db >> ciip_portal_swrs_import >> ciip_portal_deploy_data  >> ciip_portal_graphile_schema >> ciip_portal_app_user
+
+acme_issue_dag = DAG('ciip_portal_acme_issue', schedule_interval=SCHEDULE_INTERVAL, default_args=default_args)
+
+cron_acme_issue_task = PythonOperator(
+  python_callable=trigger_k8s_cronjob,
+  task_id='ciip_portal_acme_issue',
+  op_args=['cas-ciip-portal-acme-issue', namespace],
+  dag=acme_issue_dag)

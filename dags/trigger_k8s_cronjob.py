2a1df7a0c8a84e8840ab9bf2ba9babb1c4f5e2c0
Dylan Leard <dylan@button.is>
2021-06-18 15:34:03 -0700

    chore: rollback owner references change

--- dags/trigger_k8s_cronjob.py
+++ dags/trigger_k8s_cronjob.py
@@ -38,16 +38,6 @@ def trigger_k8s_cronjob(cronjob_name, namespace):
         cronjob.spec.job_template.metadata.name = str(
             date_str + cronjob.metadata.name)[:63]
 
-        # Set the job's owner_references list to allow job removal by [failed/successful]_jobs_history_limit & garbage collection
-        owner_reference = {
-            "api_version": cronjob.api_version,
-            "controller": True,
-            "kind": cronjob.kind,
-            "name": cronjob.metadata.name,
-            "uid": cronjob.metadata.uid
-        }
-        cronjob.spec.job_template.metadata.owner_references = [owner_reference]
-
         try:
             # Create a job from the job_template of the cronjob
             created_job = api.create_namespaced_job(

85be0f1b256d7e4186c3480385eb7aee10ccdd9c
Naomi Aro <naomi.aro@gov.bc.ca>
2021-07-19 15:27:35 -0700

    refactor: get and write in one task

--- dags/fetch_and_save_dag_from_github.py
+++ dags/fetch_and_save_dag_from_github.py
@@ -3,11 +3,12 @@ from airflow.utils.dates import days_ago
 from dag_configuration import default_dag_args
 import urllib.request
 import logging
+import os
 
 DAGS_FOLDER = '/opt/airflow/dags/dynamic'
 
-@dag(default_args=default_dag_args, schedule_interval=None, start_date=days_ago(2), tags=[''])
-def fetch_and_save_dags(org: str = '', repo: str = '', ref: str = '', path: str = ''):
+@dag(default_args=default_dag_args, schedule_interval=None, start_date=days_ago(2))
+def fetch_and_save_dag_from_github(org: str = '', repo: str = '', ref: str = '', path: str = ''):
     """
       DAG to fetch dags and store them to a disk location.
 
@@ -24,25 +25,21 @@ def fetch_and_save_dags(org: str = '', repo: str = '', ref: str = '', path: str 
       :type path: str
     """
 
+    def get_filepath(path):
+        file_name = path.split('/')[-1]
+        file_path = f'{os.environ["AIRFLOW_HOME"]}/dags/dynamic/{file_name}'
+        return file_path
+
     @task()
     def get_file(org, repo, ref, path):
         url = f'https://raw.githubusercontent.com/{org}/{repo}/{ref}/{path}'
         logging.critical(f'Retrieving remote DAG: {url}')
-        with urllib.request.urlopen(url) as f:
-            file = f.read()
-            return file
-
-    @task()
-    def save_file(buffer):
-        file_name = path.split('/')[-1]
-        file_path = f'/opt/airflow/dags/dynamic/{file_name}'
+        file_path = get_filepath(path)
         logging.critical(f'Saving file to disk: {file_path}')
-        with open(file_path, 'w') as desc:
-            desc.write(buffer)
+        urllib.request.urlretrieve(url, filename=file_path)
 
-    file = get_file(org, repo, ref, path)
-
-    save_file(file)
+    get_file(org, repo, ref, path)
 
 
-dag = fetch_and_save_dags()
+
+dag = fetch_and_save_dag_from_github()

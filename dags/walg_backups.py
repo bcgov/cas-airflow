0f958687f1876240b6d85cefea62d2d21e5f34df
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2021-07-28 11:27:37 -0700

    feat: adding backup task libary

---
+++ dags/walg_backups.py
@@ -0,0 +1,28 @@
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime, timedelta
+from exec_in_pod import exec_in_pod
+import os
+
+START_DATE = datetime.now() - timedelta(days=2)
+
+ciip_namespace = os.getenv('CIIP_NAMESPACE')
+ggircs_namespace = os.getenv('GGIRCS_NAMESPACE')
+in_cluster = os.getenv('LOCAL_AIRFLOW', False) == 'False'
+
+PATRONI_SELECTOR = 'spilo-role=master'
+
+full_exec_command = [
+    '/bin/sh',
+    '-c',
+    'PGPASSWORD=$PGPASSWORD_SUPERUSER envdir $WALE_ENV_DIR wal-g backup-push $PGDATA'
+]
+
+
+def create_backup_task(dag, task_name, namespace, deployment_name):
+    return PythonOperator(
+        python_callable=exec_in_pod,
+        task_id=f'make_postgres_backup_{task_name}',
+        op_args=[deployment_name, namespace,
+                 full_exec_command, PATRONI_SELECTOR],
+        dag=dag
+    )

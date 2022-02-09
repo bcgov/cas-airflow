d7643f901bf48cb7cb02c625c2f46ee120286858
Dylan Leard <dylan@button.is>
2022-02-09 15:29:09 -0800

    chore: update default email on failure notification

--- dags/dag_configuration.py
+++ dags/dag_configuration.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 default_dag_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'email': ['ggircs@gov.bc.ca'],
+    'email': ['ggircs@gov.bc.ca', 'matthieu@button.is', 'dylan@button.is', 'pierre.bastianelli@gov.bc.ca'],
     'email_on_failure': True,
     'email_on_retry': False,
     'retries': 1,

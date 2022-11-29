b44c7dccb613471cb04963927515567d5df7fd50
tmastrom <thomas.mastromonaco@gmail.com>
2022-11-29 12:42:12 -0800

    chore: add logging for exponential backoff

--- dags/trigger_k8s_cronjob.py
+++ dags/trigger_k8s_cronjob.py
@@ -6,18 +6,20 @@ import logging
 import random
 
 def retry_with_backoff(fn, retries = 6, backoff_in_seconds = 1):
-  x = 0
-  while True:
-    try:
-      return fn()
-    except:
-      if x == retries:
-        raise
+    x = 0
+    while True:
+        try:
+            return fn()
+        except:
+            if x == retries:
+                raise
 
-      sleep = (backoff_in_seconds * 2 ** x + 
-               random.uniform(0, 1))
-      time.sleep(sleep)
-      x += 1
+            sleep = (backoff_in_seconds * 2 ** x + 
+                    random.uniform(0, 1))
+            logging.critical(
+                "Retrying in {backoff} seconds".format(backoff=sleep))
+            time.sleep(sleep)
+        x += 1
 
 # Retrieves a cron_job by name & namespace
 def get_cronjob(cronjob_name, namespace, batchApi):

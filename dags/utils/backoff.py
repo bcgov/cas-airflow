e8da734d61f20f16b9d46aadf5947d37a3344b46
tmastrom <thomas.mastromonaco@gmail.com>
2022-12-09 14:32:40 -0800

    chore: make backoff recursive

--- dags/utils/backoff.py
+++ dags/utils/backoff.py
@@ -5,15 +5,13 @@ import random
 logger = logging.getLogger()
 
 def retry_with_backoff(fn, retries = 10, backoff_in_seconds = 1):
-    x = 0
-    while True:
-        try:
-            return fn()
-        except:
-            if x == retries:
-                raise
-            sleep = (backoff_in_seconds * 2 ** x +
-                    random.uniform(0, 1))
-            logger.critical("Attempt {}, retrying in {} seconds".format(x, round(sleep)))
-            time.sleep(sleep)
-        x += 1
+    try:
+        return fn()
+    except:
+        if retries <= 0:
+            raise
+        sleep = (backoff_in_seconds * 2 ** retries +
+                random.uniform(0, 1))
+        logger.critical("Retrying in {} seconds".format(round(sleep)))
+        time.sleep(sleep)
+        retry_with_backoff(fn, retries - 1, backoff_in_seconds)
